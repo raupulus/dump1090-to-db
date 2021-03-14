6d70170df6fcc9bb2ef5ae4aae57225da7a3c411
Raúl Caro Pastorino <dev@fryntiz.es>
2021-03-14 22:35:36 +0100

    Base log file create

---
+++ Helpers/Log.php
@@ -0,0 +1,24 @@
+
+<?php
+
+namespace \App\Helpers;
+
+
+class Log
+{
+    public static function error()
+    {
+
+    }
+
+    public static function success()
+    {
+
+    }
+
+    public static function info()
+    {
+
+    }
+
+}
