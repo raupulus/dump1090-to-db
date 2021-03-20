49c8b37fd0e55fb2d9f5d31717b61fb96c7c7b7f
Raúl Caro Pastorino <raul@fryntiz.dev>
2021-03-20 21:13:01 +0100

    Create while iterations for read data

--- start_dump1090_exporter.sh
+++ start_dump1090_exporter.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 
-php dump1090_exporter
+count=0
+
+while [[ true ]]
+do
+    php dump1090_exporter.php
+
+    count=$((count + 1))
+
+    sleep 10
+
+    ## Cada 10 iteraciones sube a la api todo lo que haya
+    if [[ $count = 10 ]]; then
+        echo 'Subiendo a la api'
+
+        count=0
+
+        php upload_data_to_api.php
+
+        sleep 20
+    fi
+done
